ba4080d901d29526cdcfdbd7be0b3930cd716359
Alon Sasson <03alon@walla.com>
2020-05-17 17:41:37 +0300

    Added IDatabase to server

--- Trivia_Alon_Auri/Server.h
+++ Trivia_Alon_Auri/Server.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Communicator.h"
+#include "IDatabase.h"
 
 class Server
 {
@@ -7,4 +8,5 @@ public:
 	void run(const int port);  // runs server with a given port
 private:
 	Communicator m_communicator;
+	IDatabase* m_database;
 };
