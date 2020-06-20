6d416f712b4af3d3f178e947bffac0484e2f9638
Alon Sasson <03alon@walla.com>
2020-06-20 19:51:28 +0300

    Added desrializer class for login and signup responses

---
+++ Trivia_Client/Responses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia_Client
+{
+    class Responses
+    {
+        public class LoginResponse 
+        {
+            public int status { get; set; }
+        }
+        public class SignupResponse
+        {
+            public int status { get; set; }
+        }
+    }
+}
