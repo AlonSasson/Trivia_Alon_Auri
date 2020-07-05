e34482685a83a52105425df202458110d9941f56
Alon Sasson <03alon@walla.com>
2020-07-05 20:45:27 +0300

    Added room menu and room list menu

---
+++ Trivia_Client/RoomMenu.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Trivia_Client
+{
+    partial class RoomMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoomMenu));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LogoutButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.PlayerList = new System.Windows.Forms.ListBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.LeaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(147)))), ((int)(((byte)(164)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.textBox1.Location = new System.Drawing.Point(416, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 54);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Players";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // LogoutButton
+            // 
+            this.LogoutButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(54)))), ((int)(((byte)(75)))));
+            this.LogoutButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LogoutButton.Font = new System.Drawing.Font("David", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.LogoutButton.ForeColor = System.Drawing.Color.Black;
+            this.LogoutButton.Location = new System.Drawing.Point(10, 26);
+            this.LogoutButton.Name = "LogoutButton";
+            this.LogoutButton.Size = new System.Drawing.Size(147, 54);
+            this.LogoutButton.TabIndex = 8;
+            this.LogoutButton.Text = "Log out";
+            this.LogoutButton.UseVisualStyleBackColor = false;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(54)))), ((int)(((byte)(75)))));
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("David", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.CloseButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.CloseButton.Location = new System.Drawing.Point(539, 483);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(228, 72);
+            this.CloseButton.TabIndex = 6;
+            this.CloseButton.Text = "Close Room";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            // 
+            // PlayerList
+            // 
+            this.PlayerList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(147)))), ((int)(((byte)(164)))));
+            this.PlayerList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PlayerList.Font = new System.Drawing.Font("David", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.PlayerList.FormattingEnabled = true;
+            this.PlayerList.ItemHeight = 36;
+            this.PlayerList.Items.AddRange(new object[] {
+            "Player1",
+            "Player2",
+            "Player3",
+            "Player4",
+            "Player5",
+            "Player6",
+            "Player7",
+            "Player8",
+            "Player9",
+            "Player10",
+            "Player11",
+            "Player12",
+            "Player13"});
+            this.PlayerList.Location = new System.Drawing.Point(339, 86);
+            this.PlayerList.Name = "PlayerList";
+            this.PlayerList.Size = new System.Drawing.Size(349, 360);
+            this.PlayerList.TabIndex = 5;
+            this.PlayerList.SelectedIndexChanged += new System.EventHandler(this.RoomList_SelectedIndexChanged);
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(54)))), ((int)(((byte)(75)))));
+            this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartButton.Font = new System.Drawing.Font("David", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.StartButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.StartButton.Location = new System.Drawing.Point(252, 483);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(228, 72);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "Start Game";
+            this.StartButton.UseVisualStyleBackColor = false;
+            // 
+            // LeaveButton
+            // 
+            this.LeaveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(54)))), ((int)(((byte)(75)))));
+            this.LeaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LeaveButton.Font = new System.Drawing.Font("David", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.LeaveButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LeaveButton.Location = new System.Drawing.Point(396, 483);
+            this.LeaveButton.Name = "LeaveButton";
+            this.LeaveButton.Size = new System.Drawing.Size(228, 72);
+            this.LeaveButton.TabIndex = 6;
+            this.LeaveButton.Text = "Leave Game";
+            this.LeaveButton.UseVisualStyleBackColor = false;
+            this.LeaveButton.Visible = false;
+            // 
+            // RoomMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(147)))), ((int)(((byte)(164)))));
+            this.ClientSize = new System.Drawing.Size(1004, 567);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LogoutButton);
+            this.Controls.Add(this.LeaveButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.PlayerList);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RoomMenu";
+            this.Text = "Trivia!";
+            this.Load += new System.EventHandler(this.RoomMenu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button LogoutButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.ListBox PlayerList;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button LeaveButton;
+    }
+}
